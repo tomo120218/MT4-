78cf07d93c2e3f3bd95fbb47c48f03f57765b890
tomohiro byoudou <k024g0017@g.neec.ac.jp>
2026-01-21 12:46:18 +0900

    a

--- main.cpp
+++ main.cpp
@@ -233,7 +233,7 @@ void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label)
 }
 
 
-const char kWindowTitle[] = "LE2C_20_タニタ_カイセイ";
+const char kWindowTitle[] = "MT";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
